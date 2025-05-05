d2f7cd451572eb6a0161362583912c99c88d9bf3
Tmakni <tom.makni@gmail.com>
2025-05-05 10:02:27 +0200

    ok

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,17 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Décrémenter n pour éviter une boucle infinie
+        n -= 1
     return result
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: ./factorial.py <number>")
-        sys.exit(1)
-
-    try:
-        f = factorial(int(sys.argv[1]))
-        print(f)
-    except ValueError:
-        print("Please enter a valid integer.")
-        sys.exit(1)
+f = factorial(int(sys.argv[1]))
+print(f)
